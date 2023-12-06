fc4ceeeb1dfa5e5492e2501d5114f51b839efdfe
wastu01 <adt107118@gm.ntcu.edu.tw>
2023-12-06 15:32:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,27 +18,14 @@ var chart = new Chart(ctx, {
         datasets: [{
             label: '數量',
             data: datas,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            backgroundColor: '#2194f9',
+            borderColor: '#2196f3',
             borderWidth: 1,
 
         }]  
     },
     options: {
+        responsive: true,
         layout: {
             padding: 1
         }
