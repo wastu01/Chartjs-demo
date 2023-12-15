63d91b08d4b4a1b7a289efba8cbcde3b78f348c8
wastu01 <adt107118@gm.ntcu.edu.tw>
2023-12-15 14:18:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 // Just leraning how to add motion graphic base on it
 
 
-const labels = ["time", "1", "2", "3", "4", "5"];
+
+const pinglabels = ["0", "1", "2", "3", "4", "5"];
 
 console.log(typeof labels);
 
-const datas = [1.1,2.2,3.5,4.4,5.3,6.4];
+const datas = [2.1,2.2,3.5,4.4,5.3,6.4];
 
 console.log(typeof datas);
 
 var ctx = document.getElementById('myChart').getContext('2d');
 
-var chart = new Chart(ctx, {
+var mychart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: labels ,
+        labels: pinglabels ,
         datasets: [{
             label: '數量',
             data: datas,
@@ -28,8 +29,30 @@ var chart = new Chart(ctx, {
         responsive: true,
         layout: {
             padding: 1
-        }
+        },
+        scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
     },
     // plugins: [plugin],
+
+});
+
+    // var newData = [1.1,2.2,3.5,4.4,5.3,6.4]; 
+    // var newLabel = ["6", "7", "8", "9", "10", "11"];
+
+function addData(chart, label, newData) {
+    chart.data.labels.push(label); 
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(newData); 
+    });
+    chart.update();
+}
+
+
+document.getElementById('addDataButton').addEventListener('click', function() {
+    addData(mychart, "5", [1.1]); 
 
 });
