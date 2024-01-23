6cb7b8d84c0a2bc746d2b9ba49424a214a531fdf
wastu01 <adt107118@gm.ntcu.edu.tw>
2024-01-23 09:53:18 +0800

    Update pingtest.js

--- js/pingtest.js
+++ js/pingtest.js
@@ -49,8 +49,6 @@ if(window.location.protocol === 'http:')
 else
         server = "https://" + window.location.hostname + ":8089/janus";
 
-server="http://211.73.81.36:8088/janus";
-
 console.log(server);
 
 var janus = null;
