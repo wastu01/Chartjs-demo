0ff6ee9ae1574a406ffb0067426dd5834d3e6826
wastu01 <adt107118@gm.ntcu.edu.tw>
2024-01-24 15:04:29 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -13,22 +13,24 @@ const monthNamesZh = [
 ]
 const dayNamesZh = [ '星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
 
-console.log(seconds);
-console.log(dayNamesZh[day]);
-console.log(dateObject);
-console.log(date);
-console.log(monthNamesZh[month]);
-console.log(year);
+//console.log(seconds);
+
+//console.log(dayNamesZh[day]);
+//console.log(dateObject);
+//console.log(date);
+//console.log(monthNamesZh[month]);
+//console.log(year);
 
 
 const timeInMs1 = +new Date()
 
-console.log(timeInMs1); //某個特殊時間
+//console.log(timeInMs1); //某個特殊時間
 
 
 function getCurrentSeconds() {
     const dateObject = new Date();
-    return dateObject.getSeconds();
+    return dateObject.getSeconds().toString();
+    console.log(dateObject.getSeconds());
   }
 
 
